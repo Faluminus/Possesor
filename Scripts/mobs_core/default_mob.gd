47a9b1dd393f2bf51c9080d3b199c9c2f01eac2e
Faluminus <semradfilip@email.cz>
2025-01-19 02:57:20 +0100

    health

--- Scripts/mobs_core/default_mob.gd
+++ Scripts/mobs_core/default_mob.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
+@export var override_logic: Node2D
+
+signal health_changed(new_health)
+
+var health := 100:
+	set(value):
+		health = clamp(value, 0, 100)
+		health_changed.emit(value)
+
 var speed = 80.0
 
+
 func _physics_process(delta: float) -> void:
-	
 	move_and_slide()
 
 func interact() -> void:
