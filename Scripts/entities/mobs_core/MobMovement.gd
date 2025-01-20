6de47e890b85611c41169abe90dced9b134df488
Faluminus <semradfilip@email.cz>
2025-01-20 20:28:11 +0100

    MobMov

--- Scripts/entities/mobs_core/MobMovement.gd
+++ Scripts/entities/mobs_core/MobMovement.gd
@@ -4,4 +4,13 @@ class_name MobMovement
 
 func move(character: CharacterBody2D, speed: int) -> void:
 	
+	if character.velocity.x >= Global.player_position.x:
+		character.velocity.x = -1 * speed
+	elif character.velocity.x < Global.player_position.x:
+		character.velocity.x = speed
+	
+	if character.velocity.y >= Global.player_position.y:
+		character.velocity.y = -1 * speed
+	elif character.velocity.y < Global.player_position.y:
+		character.velocity.y = speed
 	character.move_and_slide()
